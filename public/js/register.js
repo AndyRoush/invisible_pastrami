03593d9759daa416a7a16e584dda3335c400b4e9
Allanlychee <allanvly@gmail.com>
2018-05-05 19:22:02 -0700

    cleaned up register.js

--- public/js/register.js
+++ public/js/register.js
@@ -10,10 +10,7 @@ $(document).ready(function () {
     // Adding an event listener for when the form is submitted
     $(".submitBTN").on("click", function handleFormSubmit(event) {
         event.preventDefault()
-        // Condition: will not submit the post 
-        if (!firstName.val() && !lastName.val() && !userName.val() && !password.val() && !email.val()){
-            return
-        }
+        
         // Constructing a newPost object to hand to the database
         var newPost = {
             userName: userName.val(),
