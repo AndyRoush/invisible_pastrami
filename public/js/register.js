6898100eda060a881fc74cb8735bd34664539f6b
Allanlychee <allanvly@gmail.com>
2018-05-08 20:17:04 -0700

    clean up

--- public/js/register.js
+++ public/js/register.js
@@ -29,3 +29,75 @@ $(document).ready(function () {
         })
     }
 })
+
+// Login page - image slider
+$(document).ready(function () {
+
+    // slider init in signup page
+    $('.slider').slider();
+
+    //on click for create account to bring them to the set up page. 
+    $('#createAccount').click(function () {
+        window.open('register.html');
+        return false;
+    })
+
+    // On click from landing to signup
+    $('#signupButton').click(function () {
+        window.open('register.html');
+        return false;
+    })
+
+    //on click from landing page to bring existing user to log in page
+    $('#loginButton').click(function () {
+        window.open('login.html');
+        return false;
+    })
+
+    // parallax for landing page
+    $('.parallax').parallax();
+
+
+    //fade in on scroll start ===================
+    $(function () {
+        $('.logopic').fadeIn('fast');
+    });
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll(function () {
+
+        /* Check the location of each desired element */
+        $('.section').each(function (i) {
+
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object) {
+
+                $(this).animate({
+                    'opacity': '1'
+                }, 1000);
+
+            }
+
+        });
+
+    });
+    //fade in on scroll end ===============
+
+
+    //questionnaire Jquery init
+    $('select').material_select();
+    // $('select').material_select('destroy');
+
+
+ 
+
+ // Initialize collapse button
+ $(".button-collapse").sideNav();
+ // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+ //$('.collapsible').collapsible();
+       
+});
+
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/main.html"))
+    res.sendFile(path.join(__dirname, "../public/homepage.html"))
   })
 
   app.get("/login", function (req, res) {
