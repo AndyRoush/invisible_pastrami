7b3741aa0f2f07084c7abfc2436146c0564f5cf6
Allanlychee <allanvly@gmail.com>
2018-05-10 19:42:42 -0700

    getRequest user name

---
+++ public/js/client-landing.js
@@ -0,0 +1,17 @@
+// This function grabs name from the database 
+function getName() {
+    $.get("/api/posts", function (data) {
+        console.log(data)
+        var name = "Welcome " + data[data.length-1].firstName + " " + data[data.length-1].lastName + "!"
+        if (!name || !name.length) {
+            displayEmpty()
+        } else {
+            $("#usersName").append(name)
+        }
+    })
+}
+getName()
+
+function displayEmpty() {
+    $("#usersName").empty()
+}
